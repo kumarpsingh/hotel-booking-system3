0bde715ff62f10ba8f36b3791887a65f46531380
kumarpsingh <kumarpsinghin@yahoo.co.uk>
2025-11-23 18:53:39 +0530

    Add project files.

---
+++ hotel-booking-system3/Program.cs
@@ -0,0 +1,15 @@
+namespace hotel_booking_system3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
